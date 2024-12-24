4b844ee39c9becf453facc120f8a441a92c93e42
jplumail <jeanplumail76@gmail.com>
2024-12-24 02:44:36 +0100

    réduction du rayon du cercle à 3 km dans le composant Point

--- src/app/point.tsx
+++ src/app/point.tsx
@@ -6,6 +6,6 @@ export default function Point({
     lambert93ToViewBox: (lambert93: GeoJSON.Position) => GeoJSON.Position
 }) {
     const [x, y] = lambert93ToViewBox(point.coordinates);
-    const radius = 10 * 1000; // 10 km
+    const radius = 3 * 1000; // 10 km
     return <circle cx={x} cy={y} r={radius} fill="red" />;
 }
