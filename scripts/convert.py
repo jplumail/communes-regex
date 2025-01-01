1def7e646512eeaac6a6840d0b8793338c0de1dd
jplumail <jeanplumail76@gmail.com>
2025-01-01 12:53:59 +0100

    reduce size of communes.geojson

--- scripts/convert.py
+++ scripts/convert.py
@@ -19,10 +19,13 @@ gdf['geometry'] = gdf['geometry'].simplify(tolerance=tolerance, preserve_topolog
 gdf.to_file('public/regions_map.geojson', driver='GeoJSON')
 
 communes_shapefile_path = 'data/ADMIN-EXPRESS_3-2__SHP_LAMB93_FXX_2024-12-18/ADMIN-EXPRESS/1_DONNEES_LIVRAISON_2024-12-00244/ADE_3-2_SHP_LAMB93_FXX-ED2024-12-18/COMMUNE.shp'
-gdf = gpd.read_file(communes_shapefile_path)
+gdf = gpd.read_file(communes_shapefile_path, columns=["ID", "NOM", "NOM_M", "INSEE_COM", "STATUT", "POPULATION", "INSEE_CAN", "INSEE_ARR", "INSEE_DEP", "INSEE_REG", "SIREN_EPCI"])
 
 # geometry from polygon to point (centroid)
 gdf['geometry'] = gdf['geometry'].centroid
+
+# keep only ID, NOM
+gdf = gdf[["ID", "NOM", "geometry"]]
 
 # To GeoJSON
 gdf.to_file('public/communes.geojson', driver='GeoJSON')
