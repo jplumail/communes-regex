922777037c6cd1286d44c6d58dd9a3eb5a501898
jplumail <jeanplumail76@gmail.com>
2025-01-06 10:36:56 +0100

    ajout de la configuration de la carte et de la détection de Firefox

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -2,21 +2,34 @@ import { loadFrance } from './france.js';
 import { loadVilles } from './villes.js';
 import { dims, scale } from './utils.js';
 import { createDropdownList } from './search.js';
-import franceData from '/data/regions_map.geojson?raw'
-import communesData from '/data/communes.geojson?raw'
-
+import franceData from '/data/regions_map.geojson?raw';
+import communesData from '/data/communes.geojson?raw';
 
 export async function init() {
+    const map = setupMap()
+
+    loadMapData().then(([villes, france]) => {
+        map.appendChild(france);
+        map.appendChild(villes);
+    });
+
+    detectFirefox();
+}
+
+function setupMap() {
     const map = document.getElementById('map');
     map.setAttribute('viewBox', `0 0 ${(dims.maxX - dims.minX) * scale} ${(dims.maxY - dims.minY) * scale}`);
+    return map;
+}
 
-    const villes = await loadVilles(JSON.parse(communesData));
-    map.appendChild(await loadFrance(JSON.parse(franceData)));
-    map.appendChild(villes);
+async function loadMapData() {
+    const promFrance = loadFrance(JSON.parse(franceData));
+    const promVilles = loadVilles(JSON.parse(communesData)).then(villes => {
+        createDropdownList(villes.childNodes);
+        return villes;
+    });
 
-    createDropdownList(villes.childNodes);
-    
-    detectFirefox();
+    return Promise.all([promVilles, promFrance]);
 }
 
 function detectFirefox() {
